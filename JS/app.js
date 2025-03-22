af4266cf516aa521348f3314d14eb0247c158899
shivaaa07 <shivaawork07@gmail.com>
2025-03-22 13:32:51 +0530

    first commit

---
+++ JS/app.js
@@ -0,0 +1,63 @@
+let swiper = new Swiper(".myHeroSwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Best Product slider
+
+let bestProductswiper = new Swiper(".bestProductSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.50": {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+
+// Testimonial Swiper
+let testiSwiper = new Swiper(".testimonialSwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
